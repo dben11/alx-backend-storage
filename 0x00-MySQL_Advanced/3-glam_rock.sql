41a4c9f93226049d23ecec5ef628652cc21c7c96
dben <sam.dben@gmail.com>
2024-05-12 00:36:00 +0000

    old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,4 @@
 --Write a SQL scripts that lists all bands with Glam rock
 --as theair main style, ranked by their longevity
-SELECT
-	band_name,
-	(2022 - formed) AS lifespan
-FROM
-	metal_bands
-WHERE
-	main_style = 'Glam rock'
-ORDER BY
-	lifespan DESC;
+SELECT band_name,(split, 2022) - formed AS lifespan FROM metal_bands
+WHERE style LIKE  '%Glam rock%' ORDER BY lifespan DESC;
