5426f2cb8094a91cb3b536ba156704ae7684abb6
dben <sam.dben@gmail.com>
2024-05-12 18:22:53 +0000

    old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 --Write a SQL scripts that lists all bands with Glam rock
 --as theair main style, ranked by their longevity
---You should use attributes formed and split for computing the lifespan
+--You should use attributes formed and split for
+--computing the lifespan
 --Your script can be executed on any databae
 SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan 
 	FROM metal_bands
